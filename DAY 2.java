d505462884da5ff2a4ce9d8e7ddd5f77507cda53
Harsh Jain <127287026+Harshjain10020@users.noreply.github.com>
2024-01-13 20:58:24 +0530

    Create DAY 2.java
    day 2 
    i learn about boilerplate code , basic print command , variables.

---
+++ DAY 2.java
@@ -0,0 +1,17 @@
+public class JavaBasic {
+    public static void main(String arg[]) {
+        System.out.println("Hello HArsh ");
+        System.out.print("1 2 3 4 5 6 ");
+        System.out.print("Hello HArsh \n yoyo ");
+        System.out.print("1 2 3 4 5 6 ");
+        System.out.print("Hello HArsh \n yoyo \n");
+        System.out.print("****\n");
+        System.out.print("***\n");
+        System.out.print("**\n");
+        System.out.print("*\n");
+
+    }
+} 
+// boilerplate code//
+// i learn about output , variable .
+ 
