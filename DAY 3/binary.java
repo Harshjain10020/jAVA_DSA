be8484cf7022e64e25e2abad5376435e1e909ce0
Harsh Jain <127287026+Harshjain10020@users.noreply.github.com>
2024-01-18 22:25:12 +0530

    Add files via upload

---
+++ DAY 3/binary.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class binary {
+    public static void main(String args[]) {
+        /*
+        int A = 10;
+        int B = 5;
+        System.out.println("add = " + (A+B));
+        System.out.println("sub = " + (A-B));
+        System.out.println("multipy = " + (A*B));
+        System.out.println("div = " + (A/B));
+        System.out.println("mod = " + (A%B));
+        */
+        int A = 10;
+        int B = 5;
+
+       
+    }   
+}
