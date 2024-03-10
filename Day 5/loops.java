7c87aaf1a4a9d1380ef09f008de743f721acfec8
Harsh Jain <127287026+Harshjain10020@users.noreply.github.com>
2024-03-10 23:48:40 +0530

    Create loops.java

---
+++ Day 5/loops.java
@@ -0,0 +1,94 @@
+import java.util.*;
+public class loops {
+    public static void main(String[] args) {
+        // int i = 0 ;
+        // while (i <10) {
+        //     System.out.print("hello ");
+        //     i++;
+         Scanner sc = new Scanner(System.in);
+        // int n = sc.nextInt();
+        // int i = 1;
+        // sc.close();
+
+        // while (i <= n) {
+        //     System.out.println(i + " ");
+            
+        // }
+        // System.out.println();
+
+            // int n = 1 ;
+            // while (n<=10) {
+            //     System.out.print(n+" ");
+            //     n++;
+                
+            // }
+            // for(int i = 1 ;i <=10; i++ ){
+            //     System.out.println(i);
+            // }
+            // for (int i = 0 ; i<4 ; i++){
+            //     System.out.println("****");
+            // }
+            // int i = 1;
+            // while (i<4) {System.out.println("****");
+            // i++;
+            // int n = sc.nextInt();
+            // int sum = 0 ;
+            // int i = 1 ;
+            // while (i<=n) {
+            //     sum = sum + i;
+            //     i++;
+            //     System.out.println(sum);
+            // }
+            // for( int i = 1 ; i <= 5; i ++){
+            //         if(i == 3){
+            //             break;
+            //         } 
+            //         System.out.println(i);
+            // } System.out.println("out");
+            // sc.close();
+            // int i = 1;
+            // do {
+            //     System.out.println("hell0p");
+            //     i++;
+            // }
+            // while(i <=10);
+            // sc.close();
+
+            // int n = 10899;
+            // int rev = 0 ;
+            // while (n>0 ) {
+            //     int lastdigit = n%10;
+            //     rev = (rev*10)+ lastdigit;
+            //     n = n/10;
+            // }
+            // System.out.println(rev);
+            // for( int i = 1 ; i <5 ; i++){
+            //     if (i == 3){
+            //         continue;
+            //     }
+            //     System.out.println(i);
+            // do { 
+            //     System.out.println("enter number ;");
+            //     int n = sc.nextInt();
+            //     if (n%10 == 0){
+            //         continue;
+                    
+            //     }
+            //     System.out.println("number was : "+ n);
+
+            // }while (true);
+
+            for(int i = 0 ; i < 5; i ++){
+                System.out.println("Hello");
+                i+=2;
+            }
+            
+            
+            }
+
+                
+            
+        }
+    
+    
+
